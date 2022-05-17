c73a0881b399e4361e846793fc97631d594e1742
Rupasappati <rupasappati@gmail.com>
2022-05-17 18:43:04 +0530

    Added: Creat a contact Information

--- AddressBookSysytem/Program.cs
+++ AddressBookSysytem/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Address Book Program");
+using System;
+
+namespace AddressBook
+{
+    class AddressBookMain
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("Enter 1 to Add Contact");
+            int enterKey = Convert.ToInt32(Console.ReadLine());
+            switch (enterKey)
+            {
+                case 1:
+                    Contacts.AddContacts();
+                    break;
+                default:
+                    Console.WriteLine("Wrong Key. Try once more\n");
+                    AddressBookMain.Main();
+                    break;
+            }
+        }
+  
+    }
+   
+}
+
+
+
