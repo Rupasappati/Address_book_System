f6d7a55015095a768b7d64844fbc2e4689518f8f
Rupasappati <rupasappati@gmail.com>
2022-05-19 12:25:27 +0530

    Added: Ability to edit contact

--- AddressBookSysytem/CreateContacts.cs
+++ AddressBookSysytem/CreateContacts.cs
@@ -27,8 +27,6 @@ namespace AddressBook
             this.phoneNo = phoneNo;
             this.email = email;
         }
-
-        //add new contacts
         public static void AddContacts()
         {
             Console.WriteLine("Enter First Name");
@@ -49,7 +47,39 @@ namespace AddressBook
             string personEmail = Console.ReadLine();
             Contacts conObj = new Contacts(fName, sName, personAddress, personCity, personState, personZip, phoneNumber, personEmail);
             Console.WriteLine("Contact has been Added");
-            
+            Console.ReadLine();
+        }
+        public static void EditContact()
+        {
+            Console.WriteLine("Enter First Name");
+            string fName = Console.ReadLine();
+            Console.WriteLine("Enter Second Name");
+            string sName = Console.ReadLine();
+            bool personFound = false;
+            foreach (Contacts item in AddressBookMain.listCon)
+            {
+                if (((item.firstName).ToLower() == fName.ToLower()) && ((item.secondName).ToLower() == sName.ToLower()))
+                {
+                    Console.WriteLine("Enter new Address");
+                    item.address = Console.ReadLine();
+                    Console.WriteLine("Enter new City");
+                    item.city = Console.ReadLine();
+                    Console.WriteLine("Enter new State");
+                    item.state = Console.ReadLine();
+                    Console.WriteLine("Enter new Address");
+                    item.zip = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter new Phone Number");
+                    item.phoneNo = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter new Email");
+                    item.email = Console.ReadLine();
+                    personFound = true;
+                    Console.WriteLine("Details have been updated");
+                }
+            }
+            if (personFound == false)
+            {
+                Console.WriteLine("Person not found");
+            }
         }
     }
 }
--- AddressBookSysytem/Program.cs
+++ AddressBookSysytem/Program.cs
@@ -1,30 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-
-namespace AddressBook
+﻿namespace AddressBook
 {
     class AddressBookMain
     {
+        public static List<Contacts> listCon = new List<Contacts>();
         static void Main()
         {
             Console.WriteLine("Welcome to Address Book Program");
-            Console.WriteLine("Enter 1 to Add Contact");
-            int enterKey = Convert.ToInt32(Console.ReadLine());
-            switch (enterKey)
-            {
-                case 1:
-                    Contacts.AddContacts();
-                    break;
-                default:
-                    Console.WriteLine("Wrong Key. Try once more\n");
-                    AddressBookMain.Main();
-                    break;
-            }
+            
         }
-  
+
     }
-   
+
 }
-
-
-
